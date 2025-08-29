6b2cc83f16cd9a69c3a5084e7462380647d61658
beamishstuart-hue <beamishstuart@gmail.com>
2025-08-29 12:10:48 +0100

    Delete app/page.js

--- app/page.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{maxWidth:720, margin:'32px auto', padding:16}}>
-      <h1 style={{fontSize:28, fontWeight:700}}>Trip Inspire</h1>
-      <p style={{color:'#555'}}>Home route sanity check.</p>
-      <p><a href="/api/inspire" style={{textDecoration:'underline'}}>Open /api/inspire</a></p>
-    </main>
-  );
-}
